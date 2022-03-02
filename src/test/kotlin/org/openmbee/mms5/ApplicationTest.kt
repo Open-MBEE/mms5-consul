7bc55b93e3b6e1ad737c06715495e5a483ee2173
Jason Han <jason.han@jpl.nasa.gov>
2022-03-01 18:48:03 -0800

    Fix test and add gitignore

--- src/test/kotlin/org/openmbee/mms5/ApplicationTest.kt
+++ src/test/kotlin/org/openmbee/mms5/ApplicationTest.kt
@@ -3,12 +3,12 @@ package org.openmbee.mms5
 import io.ktor.http.*
 import kotlin.test.*
 import io.ktor.server.testing.*
-import org.openmbee.mms5.auth.plugins.*
+import org.openmbee.mms5.plugins.*
 
 class ApplicationTest {
     @Test
     fun testRoot() {
-        withTestApplication({ configureRouting() }) {
+        withTestApplication({ }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals("Hello World!", response.content)
